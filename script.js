aac81f88644c6e50a7df8d657d30598be227fc9b
pioneeramoafo <162882959+pioneeramoafo@users.noreply.github.com>
2024-11-10 09:48:04 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('sign-in-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Sign In button clicked!');
+    // Add your authentication logic here
+});
